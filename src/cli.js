cd3478596c55e4ac0118c485ecf7d5959b47ca23
Maximilian Klein <dasmaxl@gmail.com>
2016-04-16 22:33:00 +0200

    (chore) Add prepublish script.

---
+++ src/cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/* global __dirname, process */
+/*
+import program from 'commander'
+import fs from 'fs'
+import * as api from './api.js'
+import graphlib from 'graphlib'
+import getStdin from 'get-stdin'
+
+program
+  .version(JSON.parse(fs.readFileSync(__dirname + '/../package.json'))['version'])
+  .option('-t, --typings <file>', 'A json document containing the typings information')
+  .parse(process.argv)
+
+
+if (process.stdin.isTTY) {
+} else {
+  getStdin().then((content) => {
+    // we got something on stdin, don't open the editor
+  })
+}
+*/
