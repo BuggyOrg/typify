51817034a3c553ad6306279cac544e58408fe87e
Volker Schmidt <vschmidt@welfenlab.de>
2016-11-25 16:45:41 +0100

    added specializingEdge, generalizingEdge, collectingNode

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 
 import * as Graph from '@buggyorg/graphtools'
+
 import * as Rewrite from '@buggyorg/rewrite'
 import _ from 'lodash'
 
@@ -10,69 +11,90 @@ export function IsGenericPort (port) {
 export function TypifySpecializingEdge () {
   return Rewrite.applyEdge(
       (edge, graph) => {
-        var src = Graph.Node.port(edge.from.port, Graph.node(edge.from.node, graph))
-        console.log(JSON.stringify(src, null, 2))
-        console.log(JSON.stringify(edge.from, null, 2))
-        var dst = Graph.Node.port(edge.to.port, Graph.node(edge.to.node, graph))
-        if (Rewrite.IsGenericPort(src) === false && Rewrite.IsGenericPort(dst)) {
+        if (Rewrite.isGenericPort(edge.sourcePort) && Rewrite.isGenericPort(edge.targetPort) === false) {
           return edge
         } else {
           return false
         }
       },
       (edge, graph) => {
-        console.log('typifying specializing edge')
-        console.log(JSON.stringify(edge, null, 2))
-        var node = Graph.node(edge.from.node, graph)
-        var port = _.assign(_.cloneDeep(edge.from.port), {
-          type: edge.to.port.type
+        var line = 'typifying specializing edge from ' +
+        edge.source.name + '@' + edge.sourcePort.port +
+        ' to ' +
+        edge.target.name + '@' + edge.targetPort.port
+        console.log(line)
+        var node = Graph.node(edge.source, graph)
+        var port = _.assign(_.cloneDeep(edge.sourcePort), {
+          type: edge.targetPort.type
         })
-        return Rewrite.replacePort(node, edge.from.port, port, graph)
+        return Rewrite.replacePort(node, edge.sourcePort, port, graph)
       })
 }
 
 export function TypifyGeneralizingEdge () {
   return Rewrite.applyEdge(
       (edge, graph) => {
-        if (Rewrite.IsGenericPort(edge.from.port) && Rewrite.IsGenericPort(edge.to.port) === false) {
+        if (Rewrite.isGenericPort(edge.sourcePort) === false && Rewrite.isGenericPort(edge.targetPort)) {
           return edge
         } else {
           return false
         }
       },
       (edge, graph) => {
-        console.log('typifying generalizing edge')
-        console.log(JSON.stringify(edge, null, 2))
-        var node = Graph.node(edge.to.node, graph)
-        var port = _.assign(_.cloneDeep(edge.to.port), {
-          type: edge.from.port.type
+        var line = 'typifying generalizing edge from ' +
+        edge.source.name + '@' + edge.sourcePort.port +
+        ' to ' +
+        edge.target.name + '@' + edge.targetPort.port
+        console.log(line)
+        var node = Graph.node(edge.target, graph)
+        var port = _.assign(_.cloneDeep(edge.targetPort), {
+          type: edge.sourcePort.type
         })
-        return Rewrite.replacePort(node, edge.to.port, port, graph)
+        return Rewrite.replacePort(node, edge.targetPort, port, graph)
       })
 }
 
-export function TypifyCollectingEdge () {
-  return Rewrite.applyPort(
-      (node, port, graph) => {
-        var succ = Graph.successors(port, graph)
-        if (succ.length < 2) {
+export function TypifyCollectingNode () {
+  return Rewrite.applyNode(
+      (node, graph) => {
+        var ports = Graph.Node.inputPorts(node)
+        var genericPorts = _.filter(ports, (p) => Rewrite.isGenericPort(p))
+        var specificPorts = _.filter(ports, (p) => Rewrite.isGenericPort(p) === false)
+        if (genericPorts.length === 0) {
+          return false
+        } else if (specificPorts.length === 0) {
           return false
         }
-        var specificEdge = _.find(succ, (n) => false)
-        return specificEdge || false
+        var type = specificPorts[0].type
+        if (_.every(specificPorts, (p) => p.type === type) === false) {
+          return false
+        }
+        return {
+          node: node,
+          type: type
+        }
       },
       (match, graph) => {
-        console.log(JSON.stringify(match, null, 2))
+        var line = 'typifying collecting node ' + match.node.name
+        console.log(line)
+        var newNode = _.assign(_.cloneDeep(match.node), {
+          ports: _.map((match.node.ports), (p) => {
+            return _.assign(_.cloneDeep(p), {
+              type: match.type
+            })
+          })
+        })
+        return Graph.replaceNode(match.node, newNode, graph)
       })
 }
 
-export function TypifyDistributingEdge () {
+export function TypifyDistributingNode () {
   return Rewrite.applyNode(
       (node, graph) => false,
       (match, graph) => { })
 }
 
-export function TypifyRecursiveEdge () {
+export function TypifyRecursiveNode () {
   return Rewrite.applyNode(
       (node, graph) => false,
       (match, graph) => { })
