756a77130ad5f6cc6f54624f14b02dae0c49b0f6
Volker Schmidt <vschmidt@welfenlab.de>
2016-11-25 15:21:42 +0100

    fixed applyEdge

--- src/api.js
+++ src/api.js
@@ -1,42 +1,79 @@
 
-import {utils} from '@buggyorg/graphtools'
+import * as Graph from '@buggyorg/graphtools'
+import * as Rewrite from '@buggyorg/rewrite'
 import _ from 'lodash'
 
-export function validateTypings (typings) {
-  return !!typings && !!typings.number &&
-    !!typings.string &&
-    !!typings.bool
+export function IsGenericPort (port) {
+  return port.type === 'generic'
 }
 
-var replaceAll = (str, search, replacement) => {
-  return str.split(search).join(replacement)
+export function TypifySpecializingEdge () {
+  return Rewrite.applyEdge(
+      (edge, graph) => {
+        var src = Graph.Node.port(edge.from.port, Graph.node(edge.from.node, graph))
+        console.log(JSON.stringify(src, null, 2))
+        console.log(JSON.stringify(edge.from, null, 2))
+        var dst = Graph.Node.port(edge.to.port, Graph.node(edge.to.node, graph))
+        if (Rewrite.IsGenericPort(src) === false && Rewrite.IsGenericPort(dst)) {
+          return edge
+        } else {
+          return false
+        }
+      },
+      (edge, graph) => {
+        console.log('typifying specializing edge')
+        console.log(JSON.stringify(edge, null, 2))
+        var node = Graph.node(edge.from.node, graph)
+        var port = _.assign(_.cloneDeep(edge.from.port), {
+          type: edge.to.port.type
+        })
+        return Rewrite.replacePort(node, edge.from.port, port, graph)
+      })
 }
 
-function replaceType (str, typings) {
-  return replaceAll(
-    replaceAll(
-      replaceAll(str, 'number', typings.number),
-      'bool', typings.bool),
-    'string', typings.string)
+export function TypifyGeneralizingEdge () {
+  return Rewrite.applyEdge(
+      (edge, graph) => {
+        if (Rewrite.IsGenericPort(edge.from.port) && Rewrite.IsGenericPort(edge.to.port) === false) {
+          return edge
+        } else {
+          return false
+        }
+      },
+      (edge, graph) => {
+        console.log('typifying generalizing edge')
+        console.log(JSON.stringify(edge, null, 2))
+        var node = Graph.node(edge.to.node, graph)
+        var port = _.assign(_.cloneDeep(edge.to.port), {
+          type: edge.from.port.type
+        })
+        return Rewrite.replacePort(node, edge.to.port, port, graph)
+      })
 }
 
-function retypePorts (ports, typings) {
-  return _.mapValues(ports, _.partial(replaceType, _, typings))
+export function TypifyCollectingEdge () {
+  return Rewrite.applyPort(
+      (node, port, graph) => {
+        var succ = Graph.successors(port, graph)
+        if (succ.length < 2) {
+          return false
+        }
+        var specificEdge = _.find(succ, (n) => false)
+        return specificEdge || false
+      },
+      (match, graph) => {
+        console.log(JSON.stringify(match, null, 2))
+      })
 }
 
-function retypeNode (node, typings) {
-  return _.merge({}, node, {
-    inputPorts: retypePorts(node.inputPorts, typings),
-    outputPorts: retypePorts(node.outputPorts, typings),
-    typeHint: retypePorts(node.typeHint, typings)
-  })
+export function TypifyDistributingEdge () {
+  return Rewrite.applyNode(
+      (node, graph) => false,
+      (match, graph) => { })
 }
 
-export function applyTypings (graph, typings) {
-  if (!validateTypings(typings)) throw new Error('Cannot apply invalid typings')
-  var editGraph = utils.edit(graph)
-  editGraph.nodes = _.map(editGraph.nodes, (n) => {
-    return _.merge({}, n, {value: retypeNode(n.value, typings)})
-  })
-  return utils.finalize(editGraph)
+export function TypifyRecursiveEdge () {
+  return Rewrite.applyNode(
+      (node, graph) => false,
+      (match, graph) => { })
 }
