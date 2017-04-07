01848d95cf24bbcf5da01e3958ae846821797baf
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2017-04-07 13:58:02 +0200

    (fix) Exit with non-zero exit code on errors. Check if graph is typified after typifying.

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import * as cliExt from 'cli-ext'
-import {TypifyAll} from './api'
+import {TypifyAll, isFullyTyped} from './api'
 
 cliExt.input(process.argv[2])
 .then((graphStr) => {
@@ -10,9 +10,18 @@ cliExt.input(process.argv[2])
     graph = JSON.parse(graphStr)
   } catch (err) {
     console.error('[Typify] Cannot parse input JSON.')
+    process.exitCode = 1
+    return
   }
-  return TypifyAll(graph)
-})
-.then((res) => console.log(JSON.stringify(res, null, 2)))
-.catch((err) => console.error(err.stack || err))
+  const typifiedGraph = TypifyAll(graph)
+  if (!isFullyTyped(typifiedGraph)) {
+    console.error('[Typify] Could not typify the given graph.')
+    process.exitCode = 1
+  }
 
+  console.log(JSON.stringify(typifiedGraph, null, 2))
+})
+.catch((err) => {
+  console.error(err.stack || err)
+  process.exitCode = 1
+})
